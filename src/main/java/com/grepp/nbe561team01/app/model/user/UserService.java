393bce8589b1e5c8b9f0fa0dae41afaf7bd118df
imseohyeon <forestlight749@gmail.com>
2025-04-24 15:00:36 +0900

    feat (0424): 사용자 탈퇴 처리 기능 추가

--- src/main/java/com/grepp/nbe561team01/app/model/user/UserService.java
+++ src/main/java/com/grepp/nbe561team01/app/model/user/UserService.java
@@ -1,5 +1,24 @@
 package com.grepp.nbe561team01.app.model.user;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Transactional
+    public boolean removeUser(String email) {
+        // TODO: 삭제할 회원 정보로 수정하거나 로그 출력 삭제 필요
+        log.info("삭제할 회원의 이메일: {}", email);
+
+        userRepository.removeUser(email);
+        return true;
+    }
 }
+
