fa7cfefeee40801493ca4456280e0352767f5b12
imseohyeon <forestlight749@gmail.com>
2025-04-24 17:39:48 +0900

    test (0424): 마이페이지 회원 정보 조회 기능 테스트

--- src/test/java/com/grepp/nbe561team01/app/model/user/UserServiceTest.java
+++ src/test/java/com/grepp/nbe561team01/app/model/user/UserServiceTest.java
@@ -1,19 +1,20 @@
 package com.grepp.nbe561team01.app.model.user;
 
+import com.grepp.nbe561team01.app.model.user.dto.UserDto;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {
     "file:src/main/webapp/WEB-INF/spring/root-context.xml"
 })
 @Slf4j
-class UserServiceIntegrationTest {
+class UserServiceTest {
 
     @Autowired
     private UserService userService;
@@ -21,6 +22,11 @@ class UserServiceIntegrationTest {
     @Test
     public void removeUser(){
         userService.removeUser("aaa@aaa.com");
+    }
+
+    @Test
+    public void findUser(){
+        log.info("user: {}", userService.findByEmail("test@test.com"));
     }
 
 }
