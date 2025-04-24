08b19dfb038538efedbb0cdc2c7dd0dd0e83de72
NoviceWyatt19 <aseltula596@gmail.com>
2025-04-24 17:52:54 +0900

    feat (0424): 로그인 기능 추가

--- src/main/java/com/grepp/nbe561team01/app/model/user/UserRepository.java
+++ src/main/java/com/grepp/nbe561team01/app/model/user/UserRepository.java
@@ -1,9 +1,8 @@
 package com.grepp.nbe561team01.app.model.user;
 
 import com.grepp.nbe561team01.app.model.user.dto.UserDto;
+import java.util.Optional;
 import org.apache.ibatis.annotations.Insert;
-import com.grepp.nbe561team01.app.model.user.dto.UserDto;
-import javax.swing.text.html.Option;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -18,10 +17,15 @@ public interface UserRepository {
             + "values (#{email}, #{password}, #{role}, #{createdAt})")
     void insert(UserDto dto);
 
+
     // softDelete 적용
     @Update("update users set deleted_at = now() where email = #{email}")
     boolean removeUser(String email);
 
-    @Select("select * from users where email = #{email}")
+    @Select("select * from users where email = #{email} and deleted_at is null")
     UserDto findByEmail(String email);
+
+    @Select("select * from users where email = #{email} and deleted_at is null")
+    Optional<UserDto> selectByEmail(String email);
+
 }
